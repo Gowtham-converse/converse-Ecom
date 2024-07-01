349adeb6f2381b138c2eedb1fe08cadbae64e777
Gowtham-converse <gowthamsakthivel@conversedatasolutions.com>
2024-07-01 10:49:11 +0530

    Change the Login logic

--- project/routers/User.py
+++ project/routers/User.py
@@ -113,9 +113,12 @@ async def login_token(form_data:schemas.User_Login, db: Session = Depends(get_db
 @router.post("/request_otp")
 async def login_to_Otp(form_data:schemas.User_LoginBase, db: Session = Depends(get_db)):
     user = auth.check_exist_user_email(db, form_data.email_or_phonenumber)
-    if not user or user.is_active==0:
+    if not user:
+        
         raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Incorrect email")
     else:
+       if user.is_active==False:
+            raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED,detail="you can't Access this site")
        OTP_data=config.generate_otp_and_hash()
        OTP = OTP_data["otp"]
        hash_otp= OTP_data["hash_otp"]
